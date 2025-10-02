dc5fcadc8444b68943c198b8f8d5d60087790d97
G4ru75 <dairodu123@gmail.com>
2025-10-02 11:16:41 -0500

    agrego autologin para no tener que loguarse siempre

--- lib/controllers/usuarios_controller.dart
+++ lib/controllers/usuarios_controller.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:smart_ilumina/models/usuarios_models.dart';
 
 import '../models/usuarios_models.dart';
@@ -9,15 +10,33 @@ import '../models/usuarios_models.dart';
 class UsuariosController extends GetxController {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
+  final storage = GetStorage();
   final Rxn<Usuario> usuario = Rxn<Usuario>();
+  var isLoading = false.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    autoLogin();
+  }
+
+  void autoLogin() {
+    String? email = storage.read('email');
+    String? contrasena = storage.read('contrasena');
+    if (email != null && contrasena != null) {
+      loginUsuario(email, contrasena);
+    }
+  }
 
   Future<bool> loginUsuario(String email, String contrasena) async {
     try {
+      isLoading.value = true;
       await _auth.signInWithEmailAndPassword(
         email: email,
         password: contrasena,
       );
+      storage.write('email', email);
+      storage.write('contrasena', contrasena);
       return true;
       Get.snackbar(
         'Exito',
@@ -25,8 +44,11 @@ class UsuariosController extends GetxController {
         backgroundColor: Colors.green,
         colorText: Colors.white,
       );
+
+      isLoading.value = false;
     } on FirebaseAuthException catch (e) {
       return false;
+      isLoading.value = false;
       Get.snackbar(
         'Error',
         e.message ?? 'Error desconocido',
@@ -43,6 +65,7 @@ class UsuariosController extends GetxController {
     String contrasena,
   ) async {
     try {
+      isLoading.value = true;
       UserCredential userCredential = await _auth
           .createUserWithEmailAndPassword(email: email, password: contrasena);
 
@@ -63,6 +86,7 @@ class UsuariosController extends GetxController {
         backgroundColor: Colors.green,
         colorText: Colors.white,
       );
+      isLoading.value = false;
     } on FirebaseAuthException catch (e) {
       Get.snackbar(
         'Error',
